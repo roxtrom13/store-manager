9e63338c4b7fd8051331d66ad8e65d9b3fa960e0
Roy Palomino <roy100995@gmail.com>
2022-07-22 15:45:27 -0500

    feat: add basic sidebar, styles and animations

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,101 @@
+import { For, createSignal }  from "solid-js";
+
 export default function Navbar() {
+
+  const [selected, setSelected] = createSignal("/");
+
+  const menuItems = [
+    {
+      name: "Inicio",
+      route: "/",
+      children: null,
+    },
+    {
+      name: "Ventas",
+      route: "/sales",
+      children: [
+        {
+          name: "Crear",
+          route: "/new",
+        },
+        {
+          name: "Registros",
+          route: "/",
+        },
+      ],
+    },
+    {
+      name: "Inventario",
+      route: "/inventory",
+      children: [
+        {
+          name: "Categorías",
+          route: "/categories",
+        },
+        {
+          name: "Productos",
+          route: "/products",
+        },
+      ],
+    },
+    {
+      name: "Operaciones",
+      route: "/operations",
+      children: [
+        {
+          name: "Devoluciones y anulaciones",
+          route: "/devolutions",
+        },
+      ],
+    },
+    {
+      name: "Guías",
+      route: "/guides",
+      children: [
+        {
+          name: "Guías de ingresos",
+          route: "/income",
+        },
+        {
+          name: "Guías de salidas",
+          route: "/departure",
+        },
+      ],
+    },
+
+  ]
+
   return (
-    <div class="w-60 h-full bg-white shadow-md absolute">
-      <div class="p-6 bg-gray-100 inline-flex relative w-full">
+    <div class="w-60 h-full shadow-md absolute bg-gradient-to-b from-cyan-600 to-emerald-500 overflow-scroll">
+      <div class="p-6 inline-flex relative w-full text-center border-2 border-transparent border-b-cyan-700">
         <div class="h-2 w-2 bg-black"></div>
-        <span>Musicman</span>
+        <p class="font-sans text-2xl font-semibold text-white pl-2">Musicman</p>
       </div>
-      <ul class="relative">
-        <li class="relative">Inicio</li>
-        <li class="relative">Inventario</li>
-        <li class="relative">Ventas</li>
-        <li class="relative">Devoluciones</li>
-      </ul>
+      <div class="relative py-2 pr-1">
+      {/*SELECTED COLOR: CYAN-700*/}
+        <For each={menuItems}>{
+          (item) => (
+            <>
+              <div
+                class="border rounded p-3 border-cyan-100 border-none m-1 text-white font-semibold text-xl cursor-pointer hover:bg-cyan-600 active:bg-cyan-700"
+                classList={{"bg-cyan-700": selected() == item.route}}
+                onClick={() => setSelected(item.route)}
+              >
+                {item.name}
+              </div>
+              <ul class="ml-3 mr-1">
+                <For each={item.children}>{
+                  (child) => (
+                    <li class="text-white mb-1 font-semibold py-2 px-3 rounded-md bg-teal-600 transition-all hover:bg-cyan-600 cursor-pointer">
+                      {child.name}
+                    </li>
+                  )
+                }</For>
+              </ul>
+            </>
+          )
+        }</For>
+      </div>
     </div>
   );
 }
