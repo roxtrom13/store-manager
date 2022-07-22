7b7d2aab520e1a20402491b6b25b31f09b7adf3d
Roy Palomino <roy100995@gmail.com>
2022-07-22 17:29:32 -0500

    refactor: custom styles and nested children

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -98,20 +98,32 @@ export default function Sidebar() {
         <p class="font-sans text-2xl font-semibold text-white pl-2">Musicman</p>
       </div>
       <div class="relative py-2 pr-1">
-      {/*SELECTED COLOR: CYAN-700*/}
+        {/*@ts-ignore*/}
         <For each={menuItems}>{
-          (item) => (
+          (item: any) => (
             <>
               <div
-                class="border rounded p-3 border-cyan-100 border-none m-1 text-white font-semibold text-xl cursor-pointer hover:bg-cyan-600 active:bg-cyan-700"
+                class="border rounded p-3 border-cyan-100 border-none m-1 text-white font-semibold text-xl cursor-pointer hover:bg-cyan-600 active:bg-cyan-700 flex align-middle leading-4"
                 classList={{"bg-cyan-700": selected() == item.route}}
-                onClick={() => setSelected(item.route)}
+                onClick={() => selectItem(item)}
               >
-                {item.name}
+                <Icon path={item.icon} height="18px" class="mr-2">
+                </Icon>
+                <span class="inline">
+                  {item.name}
+                </span>
+                <Show when={!!item.children}>
+                  <Icon class="mr-2 ml-auto" path={item.open ? chevronUp : chevronDown} height="18px" />
+                </Show>
               </div>
-              <ul class="ml-3 mr-1">
+              {/*TODO: It needs to do animations*/}
+              <ul
+                class="ml-3 mr-1"
+                classList={{hidden: !item.open}}
+              >
+                {/*@ts-ignore*/}
                 <For each={item.children}>{
-                  (child) => (
+                  (child: any) => (
                     <li class="text-white mb-1 font-semibold py-2 px-3 rounded-md bg-teal-600 transition-all hover:bg-cyan-600 cursor-pointer">
                       {child.name}
                     </li>
