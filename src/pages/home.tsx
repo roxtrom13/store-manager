f4b944a86146b428963c0d481b0160320b2fbe08
Roy Palomino <roy100995@gmail.com>
2022-07-31 00:34:21 -0500

    refactor: add color and content for test

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,46 @@
 export default function Home() {
   return (
     <>
-      <h1>Bienvenido al home page!!</h1>
+      <h1>hola taonsethnaothe ntaohnsetuhnsatohenthao enth Bienvenido al home page!!</h1>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
+      <div>div de prueba</div>
     </>
   )
 }
