b3513ff3468135e4d5eb947c2af730eaafb03016
Roy Palomino <roy100995@gmail.com>
2022-07-31 00:39:31 -0500

    fix: back to fixed menu and add test text for display

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,10 +1,7 @@
 export default function Topbar() {
-  //left-24 md:left-60 w-[calc(100%-6rem)] md:w-[calc(100%-15rem)] 
   return (
-    <div class="sticky p-2 h-16 top-0 left-24 md:left-60 w-[calc(100%-6rem)] md:w-[calc(100%-15rem)] bg-cyan-600">
-      <p class="text-white text-xl">
-        Top bar content
-      </p>
+    <div class="sticky p-2 h-16 top-0 ml-24 md:ml-60 z-30 w-[calc(100% - 6rem)] md:w-[calc(100% - 15rem)] bg-cyan-600">
+      <p class="text-white text-2xl">Topbar content here!</p>
     </div>
   )
 }
