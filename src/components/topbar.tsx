d08eea7b04332cfb696f0a1772bcf91acc1a5fc9
Roy Palomino <roy100995@gmail.com>
2022-07-31 00:33:28 -0500

    feat: add responsive top bar to display app info

---
+++ src/components/topbar.tsx
@@ -0,0 +1,10 @@
+export default function Topbar() {
+  //left-24 md:left-60 w-[calc(100%-6rem)] md:w-[calc(100%-15rem)] 
+  return (
+    <div class="sticky p-2 h-16 top-0 left-24 md:left-60 w-[calc(100%-6rem)] md:w-[calc(100%-15rem)] bg-cyan-600">
+      <p class="text-white text-xl">
+        Top bar content
+      </p>
+    </div>
+  )
+}
