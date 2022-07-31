2d4677e5c42d53ab5e2ac01492df67c1b2e4633b
Roy Palomino <roy100995@gmail.com>
2022-07-31 00:34:06 -0500

    refactor: change display settings and add topbar

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,5 +1,6 @@
 import { lazy } from "solid-js";
 import SideBar from "../components/sidebar";
+import Topbar from "../components/topbar";
 import { Routes, Route } from "solid-app-router";
 
 const Home = lazy(() => import("../pages/home"));
@@ -7,16 +8,15 @@ const Sales = lazy(() => import("../pages/sales"));
 
 export default function Default() {
   return (
-    <div class="flex flex-row">
-      <div>
-        <SideBar></SideBar>
-      </div>
-      <div class="ml-60 w-full">
+    <div class="relative min-h-screen w-full">
+      <SideBar />
+      <Topbar />
+      <section class="p-2 relative min-h-screen left-24 md:left-60 w-[calc(100%-6rem)] md:w-[calc(100%-15rem)] z-10 bg-pink-400">
         <Routes>
           <Route path="/" component={Home} />
           <Route path="/sales" component={Sales} />
         </Routes>
-      </div>
+      </section>
     </div>
   )
 }
